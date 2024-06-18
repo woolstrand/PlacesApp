f6d1986af4f2cdf25f0c47192ce6ab4109082fd4
Igor Chertenkov <woolstrand@gmail.com>
2024-06-18 10:29:29 +0200

    Initial Commit

---
+++ LocationsList/LocationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationsListApp.swift
+//  LocationsList
+//
+//  Created by Igor Chertenkov on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
